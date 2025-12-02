4715dda726445268f11bbfb6c701e0ae829bd07d
User <user@192.168.1.2>
2025-12-02 10:04:24 -0500

    url render

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   //apiUrl: 'http://192.168.1.2:3306/api',
-  apiUrl: 'https://wy1bv3-ip-181-55-23-235.tunnelmole.net/api'
+  //apiUrl: 'https://wy1bv3-ip-181-55-23-235.tunnelmole.net/api'
+  apiUrl: 'https://backend-papaya.onrender.com/api'
 };
