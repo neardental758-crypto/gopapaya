b7ab7d9e24788f8f1347e298fb97c2ea536cdd76
YesidWork2804 <yesidwork2804@gmail.com>
2026-01-21 00:15:21 -0500

    actualizacion variable de entorno

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   //apiUrl: 'http://192.168.1.2:3306/api', api local
   //apiUrl: 'https://wy1bv3-ip-181-55-23-235.tunnelmole.net/api'
-  // apiUrl: 'https://backend-papaya.onrender.com/api',
-  apiUrl: 'http://localhost:3002/api',
+  apiUrl: 'https://backend-papaya.onrender.com/api',
+  // apiUrl: 'http://localhost:3002/api',
 };
